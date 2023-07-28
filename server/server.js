94cd25df5dea498edeefa9f02f1fb11254f0d944
KT <132851569+kumih0@users.noreply.github.com>
2023-07-27 20:16:17 -0500

    copied in express setup and build paths

--- server/server.js
+++ server/server.js
@@ -14,4 +14,15 @@ const server = new ApolloServer({
     resolvers,
     context: authMiddleware,
 });
+ 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+};
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
