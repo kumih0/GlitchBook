e4bd0c4093ab0a8e210361aaeaa17cc88f1c545c
KT <132851569+kumih0@users.noreply.github.com>
2023-07-28 08:41:32 -0500

    created new server file with code from doc

--- server/server.js
+++ server/server.js
@@ -1,40 +1,30 @@
-const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { authMiddleware } = require('./utils/auth');
-const path = require('path');
-const db = require('./config/connections');
+// npm install @apollo/server express graphql cors body-parser
+//copied from apollo server v4 documentation
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { json } from 'body-parser';
+import { typeDefs, resolvers } from './schema';
 
-const { typeDefs, resolvers } = require('./schemas');
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
+app.use(
+  '/graphql',
+  cors(),
+  json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  }),
+);
 
-async function startApolloServer() {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware, // Assuming `authMiddleware` returns the context object
-  });
-
-  await server.start();
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
-
-  server.applyMiddleware({ app });
-
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
-  });
-}
-
-// calling startApolloServer
-startApolloServer();
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
