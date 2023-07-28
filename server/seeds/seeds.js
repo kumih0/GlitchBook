c20d4696642a2c5d3d811f332796968d955d2e69
HamzaR19 <132932060+HamzaR19@users.noreply.github.com>
2023-07-28 13:16:54 -0500

    aking changes to dummy for dummies

---
+++ server/seeds/seeds.js
@@ -0,0 +1,34 @@
+const db = require('../config/connections');
+const { User, Post, Comment } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const postSeeds = require('./postSeeds.json');
+
+db.once('open', async () => {
+  try {
+    await Post.deleteMany({});
+    await User.deleteMany({});
+    // await Comment.deleteMany({});
+    
+    await User.create(userSeeds);
+
+    for (let i = 0; i < postSeeds.length; i++) {
+      const { _id, username } = await Post.create(postSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: username },
+        {
+          $addToSet: {
+          
+            posts : _id,
+            postTitle: postSeeds[i].postTitle,
+          },
+        }
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('all done!');
+  process.exit(0);
+});
