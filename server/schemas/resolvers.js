6a26f21280b3d725b794b7d76815307361a1b5ea
KT <132851569+kumih0@users.noreply.github.com>
2023-07-27 23:36:44 -0500

    add user and login mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -30,4 +30,28 @@ const resolvers = {
             return await Post.find({ _id: postId });
         },
     },
-    
+    Mutation: {
+        //add user mutation
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        //login mutation
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
