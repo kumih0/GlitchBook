9e067929271ba0cfb9dbb2435a3ebef52eb40097
KT <132851569+kumih0@users.noreply.github.com>
2023-08-03 00:24:00 -0500

    copying from past class activities

--- client/src/pages/ProfilePage/ProfilePage.js
+++ client/src/pages/ProfilePage/ProfilePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navbar, ProfileData, FriendsList, Post, PostForm } from '../../components';
 import { useQuery } from '@apollo/client';
 import { GET_ME, GET_USER } from '../../utils/queries';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 import Auth from '../../utils/auth';
 import './style/ProfilePage.css';
 
@@ -15,6 +15,23 @@ const ProfilePage = () => {
   });
 
   const user = data?.me || data?.user || {};
+  // navigate to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/me" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
+    );
+  }
 
   return (
     <div className="profile-page-container">
