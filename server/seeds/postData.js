881c06cd1e8124cd98c70cc3f996d48c5f435e24
KT <132851569+kumih0@users.noreply.github.com>
2023-08-17 15:41:21 -0500

    added more post seed data

--- server/seeds/postData.js
+++ server/seeds/postData.js
@@ -1,44 +1,284 @@
 const posts = [
-    {
-      postTitle: "The best thing about a boolean is even if you are wrong, you are only off by a bit.",
-      postText: "Everyone knows that debugging is twice as hard as writing a program in the first place. So if you're as clever as you can be when you write it, how will you ever debug it?"
-    },
-    {
-      postTitle: "Coding Masterpiece: Scribbled on Paper, Lost in Code",
-      postText: "Some of the best programming is done on paper, really. Putting it into the computer is just a minor detail."
-    },
-    {
-      postTitle: "Inventor of Bugs, Taster of Errors: A Creative Journey",
-      postText: "When you don't create things, you become defined by your tastes rather than ability. Your tastes only narrow & exclude people. So create."
-    },
-    {
-      postTitle: "Confessions of a Good Programmer with Great Habits",
-      postText: "I'm not a great programmer; I'm just a good programmer with great habits."
-    },
-    {
-      postTitle: "Frozen Code Chronicles: Walking on Water and Debugging Ice",
-      postText: "Walking on water and developing software from a specification are easy if both are frozen."
-    },
-    {
-      postTitle: "The Comedy of Coding Errors: My First Programming Language",
-      postText: "The most disastrous thing that you can ever learn is your first programming language."
-    },
-    {
-      postTitle: "Debugging Life: The Intentions of a Programmer",
-      postText: "The most important property of a program is whether it accomplishes the intention of its user."
-    },
-    {
-      "postTitle": "Shipping Bugs: From Imperfect to Delightful Software",
-      "postText": "Delivering good software today is often better than perfect software tomorrow, so finish things and ship."
-    },
-    {
-      postTitle: "The Optimization Trap: Chasing Perfection in Code",
-      postText: "If you optimize everything, you will always be unhappy."
-    },
-    {
-      postTitle: "The 'Undocumented Feature' Chronicles: A Programmer's Excuse",
-      postText: "It's not a bug; it's an undocumented feature."
-    }
+  {
+    postTitle: "The best thing about a boolean is even if you are wrong, you are only off by a bit.",
+    postText: "Everyone knows that debugging is twice as hard as writing a program in the first place. So if you're as clever as you can be when you write it, how will you ever debug it?"
+  },
+  {
+    postTitle: "Coding Masterpiece: Scribbled on Paper, Lost in Code",
+    postText: "Some of the best programming is done on paper, really. Putting it into the computer is just a minor detail."
+  },
+  {
+    postTitle: "Inventor of Bugs, Taster of Errors: A Creative Journey",
+    postText: "When you don't create things, you become defined by your tastes rather than ability. Your tastes only narrow & exclude people. So create."
+  },
+  {
+    postTitle: "Confessions of a Good Programmer with Great Habits",
+    postText: "I'm not a great programmer; I'm just a good programmer with great habits."
+  },
+  {
+    postTitle: "Frozen Code Chronicles: Walking on Water and Debugging Ice",
+    postText: "Walking on water and developing software from a specification are easy if both are frozen."
+  },
+  {
+    postTitle: "The Comedy of Coding Errors: My First Programming Language",
+    postText: "The most disastrous thing that you can ever learn is your first programming language."
+  },
+  {
+    postTitle: "Debugging Life: The Intentions of a Programmer",
+    postText: "The most important property of a program is whether it accomplishes the intention of its user."
+  },
+  {
+    "postTitle": "Shipping Bugs: From Imperfect to Delightful Software",
+    "postText": "Delivering good software today is often better than perfect software tomorrow, so finish things and ship."
+  },
+  {
+    postTitle: "The Optimization Trap: Chasing Perfection in Code",
+    postText: "If you optimize everything, you will always be unhappy."
+  },
+  {
+    postTitle: "The 'Undocumented Feature' Chronicles: A Programmer's Excuse",
+    postText: "It's not a bug; it's an undocumented feature."
+  },
+  {
+    postTitle: "Just got that promotion!",
+    postText: "Feeling on top of the world right now. Hard work pays off! 🎉 #CareerGoals"
+  },
+  {
+    postTitle: "Chasing sunsets around the world.",
+    postText: "Every sunset is a reminder of life's beauty. 🌅✈️ #Wanderlust"
+  },
+  {
+    postTitle: "Savoring every bite of this mouthwatering dish!",
+    postText: "Foodie adventure: indulging in the most amazing flavors. 🍔😋 #FoodLove"
+  },
+  {
+    postTitle: "Morning workout done!",
+    postText: "Starting the day with a sweat session. Feeling strong and motivated! 💪 #FitnessLife"
+  },
+  {
+    postTitle: "Diving into this captivating novel.",
+    postText: "Books have the power to transport us to different worlds. 📚🤓 #BookNerd"
+  },
+  {
+    postTitle: "Jamming to the latest beats.",
+    postText: "Music is my escape. Let the rhythm take over! 🎶🎧 #MusicAddict"
+  },
+  {
+    postTitle: "Just climbed to the top of the mountain!",
+    postText: "Nature's beauty is worth every step. The view is unreal. 🏞️ #NatureLover"
+  },
+  {
+    postTitle: "Starting the day with a cup of coffee and good vibes.",
+    postText: "Nothing beats the warmth of a morning brew. ☕️🌞 #CoffeeLover"
+  },
+  {
+    postTitle: "New outfit, who dis?",
+    postText: "Rocking this look with confidence and style. Fashion is my passion. 👗 #FashionGoals"
+  },
+  {
+    postTitle: "Exploring the latest gadgets and tech innovations.",
+    postText: "Tech world, here I come! Discovering the future of technology. 📱🔌 #TechLife"
+  },
+  {
+    postTitle: "Chasing dreams, one step at a time.",
+    postText: "Life's a journey filled with endless possibilities. Keep pushing forward! 🌟 #DreamBig"
+  },
+  {
+    postTitle: "Reflecting on a Year of Growth",
+    postText: "As I look back on the past year, I'm amazed at how much I've grown. From overcoming challenges to embracing new opportunities, it's been a journey of self-discovery and resilience. Grateful for every moment that shaped me into who I am today. Here's to more growth and adventures ahead! 🌱💪 #PersonalGrowth"
+  },
+  {
+    postTitle: "Taking Action for Change",
+    postText: "In a world where injustices persist, it's crucial to take a stand. I joined a local community initiative advocating for equal access to education. Every step counts towards creating a more equitable society. Let's amplify voices and create lasting change together. ✊📚 #SocialChange"
+  },
+  {
+    postTitle: "Quality Time with the Fam",
+    postText: "Spent the weekend surrounded by family, sharing stories, laughter, and delicious home-cooked meals. There's something magical about reconnecting with loved ones and reliving cherished memories. Grateful for these moments that remind me of what truly matters. ❤️👨‍👩‍👧‍👦 #FamilyTime"
+  },
+  {
+    postTitle: "Exploring New Book Horizons",
+    postText: "Diving into the latest novel that's been making waves. The characters are so relatable, and the themes hit close to home. Fiction has this incredible power to open our minds and connect us through shared experiences. Can't wait to see how the story unfolds! 📚✨ #ReadingJourney"
+  },
+  {
+    postTitle: "Unplugging and Reconnecting",
+    postText: "Took a digital detox weekend to recharge and focus on the present. The quiet moments spent in nature were a refreshing reminder of life's simplicity. In this age of constant connectivity, it's essential to unplug and reconnect with our surroundings. 🌿🌄 #DigitalDetox"
+  },
+  {
+    postTitle: "Embracing the Streaming Era",
+    postText: "Binge-watched the latest series that everyone's talking about. The storytelling, character development, and unexpected plot twists kept me hooked from start to finish. In a world of endless content, it's exciting to explore new narratives. 📺🍿 #TVSeriesMarathon"
+  },
+  {
+    postTitle: "Making a Difference, One Step at a Time",
+    postText: "Joined a local cleanup initiative to help protect the environment. With each piece of litter picked up, I'm reminded of the collective responsibility we have to care for our planet. Change starts small, but it ripples into something greater. ♻️🌎 #EnvironmentalAction"
+  },
+  {
+    postTitle: "Celebrating the Small Wins",
+    postText: "Today, I conquered a task that seemed daunting. It's important to recognize and celebrate even the smallest victories. They add up and contribute to our personal growth and resilience. Here's to more accomplishments on this journey! 🏆🌟 #PersonalAchievements"
+  },
+  {
+    postTitle: "Media Influence and Cultural Conversations",
+    postText: "The latest documentary on social media's impact got me thinking about the role platforms play in shaping our perceptions and conversations. It's a reminder to consume media mindfully and engage in discussions that promote critical thinking and empathy. 📽️💬 #MediaInfluence"
+  },
+  {
+    postTitle: "Time Flies: Reflecting on Milestones",
+    postText: "Can't believe it's been a year since that unforgettable trip. Looking back, the memories and experiences are still so vivid. Time has a way of reminding us to savor every moment and cherish the adventures that shape our lives. 🌍🗺️ #TravelMemories"
+  },
+  {
+    postTitle: "Embracing the hustle and grind.",
+    postText: "Success isn't handed to you. It's earned through hard work and dedication. 💪 #Hustle"
+  },
+  {
+    postTitle: "Epic adventure with my squad!",
+    postText: "Exploring new places and making memories with the best crew. 🌍👯‍♂️ #AdventureTime"
+  },
+  {
+    postTitle: "Movie night at home!",
+    postText: "Snacks, comfy blankets, and a great film. Perfect way to unwind. 🍿📺 #MovieNight"
+  },
+  {
+    postTitle: "Weekend vibes are in full swing!",
+    postText: "Time to relax, recharge, and enjoy life's simple pleasures. 🌴🎉 #WeekendVibes"
+  },
+  {
+    postTitle: "Feeling inspired by the city lights.",
+    postText: "Urban exploration is an art form in itself. Capturing the essence of the cityscape. 🌆📸 #UrbanAdventures"
+  },
+  {
+    postTitle: "Chasing waterfalls and wild adventures.",
+    postText: "Nature's playground: discovering hidden gems and untamed beauty. 🏞️🌊 #NatureSeeker"
+  },
+  {
+    postTitle: "Pizza party with the squad!",
+    postText: "Nothing brings people together like good food and great company. 🍕🎉 #PizzaNight"
+  },
+  {
+    postTitle: "Embracing change and new beginnings.",
+    postText: "Life is full of chapters waiting to be written. Embrace the journey! 📖🌟 #NewBeginnings"
+  },
+  {
+    postTitle: "Sunday vibes: self-care and relaxation.",
+    postText: "Taking time for myself and indulging in some much-needed pampering. 🛁💆‍♀️ #SelfCareSunday"
+  },
+  {
+    postTitle: "Connecting Through Shared Hobbies",
+    postText: "Joined a photography club and instantly bonded with fellow enthusiasts. It's incredible how a shared passion can create meaningful connections and lasting friendships. Excited to learn and grow together! 📷👫 #PhotographyCommunity"
+  },
+  {
+    postTitle: "Navigating Work-Life Balance",
+    postText: "Finding that delicate balance between work and personal life is an ongoing journey. It's important to prioritize self-care and downtime, ensuring we're equipped to give our best in all aspects of life. Here's to achieving harmony in the chaos! ⚖️🕰️ #WorkLifeBalance"
+  },
+  {
+    postTitle: "Sparking Conversations on Mental Health",
+    postText: "Attended an insightful panel discussion on destigmatizing mental health. Open conversations are crucial for fostering understanding and support. Let's continue breaking down barriers and promoting a culture of empathy and acceptance. 🗣️💚 #MentalHealthMatters"
+  },
+  {
+    postTitle: "Rediscovering the Joy of Art",
+    postText: "Started painting again after a hiatus, and it's like reuniting with an old friend. Art allows us to express emotions and perspectives in a unique way. Grateful for creative outlets that bring happiness and fulfillment. 🎨✨ #ArtisticJourney"
+  },
+  {
+    postTitle: "Adventures Beyond the Screen",
+    postText: "Took a break from virtual experiences and explored the outdoors. Disconnecting from technology and embracing the natural world is a reminder of the beauty that surrounds us. Nature's wonders inspire awe and gratitude. 🌳🌼 #NatureExploration"
+  },
+  {
+    postTitle: "Evolving Perspectives on Wellness",
+    postText: "Wellness is more than just physical health; it's a holistic journey that encompasses mental, emotional, and spiritual aspects. Embracing practices like mindfulness and gratitude is a step towards overall well-being and inner peace. 🌿🧘‍♀️ #HolisticWellness"
+  },
+  {
+    postTitle: "Exploring local markets and hidden gems.",
+    postText: "Supporting local businesses while discovering unique treasures. 🛍️🏺 #ShopLocal"
+  },
+  {
+    postTitle: "Road trip with endless possibilities.",
+    postText: "Hitting the open road and letting the adventure unfold. 🚗🛣️ #RoadTrip"
+  },
+  {
+    postTitle: "Epic night out with my crew!",
+    postText: "Dancing the night away and making unforgettable memories. 🕺👯‍♀️ #NightLife"
+  },
+  {
+    postTitle: "Home sweet home vibes.",
+    postText: "There's no place like home, where comfort and coziness reside. 🏡❤️ #HomeSweetHome"
+  },
+  {
+    postTitle: "Unwinding with a good movie.",
+    postText: "Popcorn, pajamas, and a great film - the ultimate relaxation combo. 🍿🎬 #MovieTime"
+  },
+  {
+    postTitle: "Chasing dreams under the starlit sky.",
+    postText: "Stargazing and pondering the vastness of the universe. ✨🌌 #StarryNight"
+  },
+  {
+    postTitle: "Weekend getaway to recharge.",
+    postText: "Escaping the daily routine to find serenity and adventure elsewhere. 🌄🌴 #WeekendEscape"
+  },
+  {
+    postTitle: "Sunrise hike for a fresh start.",
+    postText: "Witnessing the dawn and embracing the promise of a new day. 🌅🚶‍♀️ #SunriseAdventure"
+  },
+  {
+    postTitle: "Spreading positive vibes and smiles.",
+    postText: "A smile can brighten someone's day and make the world a better place. 😄🌈 #SpreadLove"
+  },
+  {
+    postTitle: "Celebrating little victories along the way.",
+    postText: "Life is made up of small triumphs that deserve recognition and applause. 🎉🏆 #SmallWins"
+  },
+  {
+    postTitle: "Exploring Culinary Delights",
+    postText: "Ventured into the world of international cuisine and tried dishes that awakened my taste buds. Food has this incredible ability to transport us to different cultures and connect us through shared flavors. 🍽️🌍 #FoodExploration"
+  },
+  {
+    postTitle: "Nurturing Creativity Through Challenges",
+    postText: "Participated in a 30-day creative challenge and pushed the boundaries of my artistic abilities. Challenges provide a platform to experiment, learn, and evolve. Embracing discomfort is often the path to personal growth. 🎨🚀 #CreativeJourney"
+  },
+  {
+    postTitle: "Building Bridges Across Generations",
+    postText: "Engaged in a heartfelt conversation with grandparents, sharing stories from different eras. These intergenerational connections remind us of the wisdom that comes with experience and the beauty of passing down traditions. 👵👴❤️ #FamilyLegacy"
+  },
+  {
+    postTitle: "Advocating for Climate Action",
+    postText: "Attended a climate rally to raise awareness about the urgency of environmental conservation. It's essential to stand up for our planet and demand action for a sustainable future. Let's be the voice of change. 🌍🌱 #ClimateJustice"
+  },
+  {
+    postTitle: "Rediscovering the Joy of Reading",
+    postText: "Rekindled my love for reading with a diverse range of books that transport me to different worlds. Books are passports to knowledge, empathy, and imagination. Let's continue exploring the written word! 📚✨ #BookwormAdventures"
+  },
+  {
+    postTitle: "Thriving in the Freelance Journey",
+    postText: "Embarked on a freelance career journey, embracing the freedom to pursue passions and work on projects that resonate. Freelancing brings its challenges and rewards, reminding us that the pursuit of dreams is worth it. 💼🌟 #FreelanceLife"
+  },
+  {
+    postTitle: "Celebrating Cultural Diversity",
+    postText: "Attended a multicultural festival that showcased the vibrant tapestry of cultures within our community. Celebrating diversity fosters understanding and unity, reminding us of the beauty in our differences. 🌎🎉 #CulturalHarmony"
+  },
+  {
+    postTitle: "Fostering Growth Mindset",
+    postText: "Embracing challenges as opportunities for growth. With a growth mindset, setbacks become stepping stones toward success. The journey is about learning, adapting, and becoming the best version of ourselves. 🌱🧠 #GrowthMindset"
+  },
+  {
+    postTitle: "Bridging Generations Through Music",
+    postText: "Introduced my favorite music to older family members and discovered shared appreciation for melodies that transcend time. Music connects generations, reminding us that some things are universally cherished. 🎶❤️ #MusicalConnections"
+  },
+  {
+    postTitle: "Capturing Moments of Serenity",
+    postText: "Spent the afternoon by the waterfront, soaking in the tranquility and reflecting on life's journey. Moments of solitude allow us to find clarity amidst the chaos and appreciate the beauty of the present. 🌊🌞 #SerenityNow"
+  },
+  {
+    postTitle: "Embracing the Journey of Parenthood",
+    postText: "Welcomed a new addition to the family and embarked on the adventure of parenthood. It's a journey of unconditional love, selflessness, and growth. Here's to the joys and challenges that come with raising a child! 👶👪 #Parenthood"
+  },
+  {
+    postTitle: "Celebrating the Beauty of Nature",
+    postText: "Took a walk in the park and marveled at the wonders of nature. The trees, flowers, and wildlife are a reminder of the beauty that surrounds us. Let's continue protecting our planet and preserving its treasures. 🌳🌼 #NatureLover"
+  },
+  {
+    postTitle: "Exploring the World of Podcasts",
+    postText: "Discovered a new podcast that's both entertaining and informative. Podcasts are a great way to learn, grow, and connect with others. Let's continue exploring the world of audio storytelling! 🎙️🎧 #PodcastJourney"
+  },
+  {
+    postTitle: "Celebrating the Joy of Friendship",
+    postText: "Spent the day with friends, sharing stories, laughter, and delicious food. Friendships are a source of joy, comfort, and support. Let's continue nurturing these connections that make life beautiful. 👯‍♀️👯‍♂️ #FriendshipGoals"
+  },
 ];
 
 module.exports = posts;
